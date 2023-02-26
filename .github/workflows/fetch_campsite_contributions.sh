c9984c62836f71fc90809edfa84de61b091669ed
Nick Holden <nick.r.holden@gmail.com>
2023-02-26 09:17:54 -0700

    Add workflow for fetching Campsite contributions

---
+++ .github/workflows/fetch_campsite_contributions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rss=$(curl -s https://www.campsite.design/changelog/rss)
+urls=( $(xmllint --xpath "//item/link/text()" - <<<"$rss") )
+result_json="[]"
+
+for url in ${urls[@]}; do
+  page=$(curl -s $url)
+  if (grep -q nick.jpeg <<<"$page")
+  then
+    page_title=$(xmllint --xpath "//title/text()" - <<<"$page" | sed 's/ · Campsite//g')
+    result_json=$(jq --arg title "$page_title" --arg url "$url" '.[. | length] |= . + {"title": $title,"url": $url}' <<<"$result_json")
+  fi
+done
+
+echo $result_json
